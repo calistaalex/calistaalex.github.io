816c87208a47cb229b707ecb055925da6ef465da
Calista Alex <calista_alex@sfu.ca>
2022-11-25 02:48:43 -0800

    Added Header
    Added Header into Style Guide

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+/*
+MAIN [P03: Portfolio Project]
+By: Calista Alex
+*/
 
+const body = document.body;
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const headerHeight = document.querySelector("header").offsetHeight;
+
+main.style.top = headerHeight + "px";
+
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  let currentScroll = window.pageYOffset;
+  // console.log("current: ", currentScroll);
+  // console.log("last: ", lastScroll);
+  if (currentScroll - lastScroll > 0) {
+    header.classList.add("scroll-down");
+    header.classList.remove("scroll-up");
+  }
+  else {
+    header.classList.add("scroll-up");
+    header.classList.remove("scroll-down");
+  }
+  lastScroll = currentScroll;
+  // console.log("last: ", lastScroll);
+});
