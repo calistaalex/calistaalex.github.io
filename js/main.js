3b3360e5bf469cf4a38e60afd287d481bde14029
Calista Alex <calista_alex@sfu.ca>
2022-12-02 07:27:43 -0800

    Updated Content
    Updated Content in All Pages

--- js/main.js
+++ js/main.js
@@ -158,7 +158,7 @@ var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
         this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
+        this.period = parseInt(period, 0.01) || 0.5;
         this.txt = '';
         this.tick();
         this.isDeleting = false;
